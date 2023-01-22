224c88d87760f6b39344c46fdc77e739fdd6c8f5
user1129 <109167316+user1129@users.noreply.github.com>
2023-01-22 16:44:25 +0600

    Add files via upload

---
+++ internal/data/models.go
@@ -0,0 +1,24 @@
+package data
+
+import (
+	"errors"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+type Models struct {
+	Movies MovieModel
+	Users  UserModel
+}
+
+func NewModels(db *pgxpool.Pool) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
+	}
+}
